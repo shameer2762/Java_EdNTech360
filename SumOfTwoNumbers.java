542c4a28783183393e8d6e571e73b55dcb35c938
shameer2762 <shaikshameer2762@gmail.com>
2023-11-26 17:12:35 +0530

    commiting sum of two numbers with different format specifiers

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -6,15 +6,30 @@ public class SumOfTwoNumbers {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter input a : ");
         //int a = sc.nextInt();
-        float a = sc.nextFloat();
+        //float a = sc.nextFloat();
+        //short a = sc.nextShort();
+        //byte a = sc.nextByte();
+        long a = sc.nextLong();
+
         System.out.print("Enter input b : ");
+
+
         //int b = sc.nextInt();
-        float b = sc.nextFloat();
+        //float b = sc.nextFloat();
+        //short b = sc.nextShort();
+        //byte b = sc.nextByte();
+        long b = sc.nextLong();
 
         //int sum = a + b;
-        float sum = a+b;
+        //float sum = a+b;
+        //short sum = (short)(a + b);
+        //byte sum = (byte)(a + b);
+        long sum = a + b ;
 
         //System.out.printf("sum of %d and %d is %d ",a,b,sum);
-        System.out.printf("sum of %f and %f is %f", a,b,sum);
+        //System.out.printf("sum of %f and %f is %f", a,b,sum);
+        //System.out.printf("sum of %h and %h is %h", a,b,sum);
+        //System.out.printf("sum of %h and %h is %h",a,b,sum);
+        System.out.printf("sum of %dl and %dl is %dl",a,b,sum);
     }
 }
