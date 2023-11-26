f40fdbe4e01316a36f9aa352804eb561de859cb4
shameer2762 <shaikshameer2762@gmail.com>
2023-11-26 16:49:08 +0530

    dependent file

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -5,12 +5,16 @@ public class SumOfTwoNumbers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter input a : ");
-        int a = sc.nextInt();
+        //int a = sc.nextInt();
+        float a = sc.nextFloat();
         System.out.print("Enter input b : ");
-        int b = sc.nextInt();
+        //int b = sc.nextInt();
+        float b = sc.nextFloat();
 
-        int sum = a + b;
+        //int sum = a + b;
+        float sum = a+b;
 
-        System.out.printf("sum of %d and %d is %d ",a,b,sum);
+        //System.out.printf("sum of %d and %d is %d ",a,b,sum);
+        System.out.printf("sum of %f and %f is %f", a,b,sum);
     }
 }
