a73d36fb0fe4fbcad4c4037d7a510beac02c0e02
shameer2762 <shaikshameer2762@gmail.com>
2023-11-26 18:26:42 +0530

    final version of sum of two numbers

--- SumOfTwoNumbers.java
+++ SumOfTwoNumbers.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class SumOfTwoNumbers {
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter input a : ");
         //int a = sc.nextInt();
         //float a = sc.nextFloat();
@@ -42,6 +44,8 @@ public class SumOfTwoNumbers {
         String str1 = sc.next();
         System.out.println(str1);
 //********************** Reading complete string ******************
+
+
         System.out.print("Enter complete string : \n");
         String str2 = sc.nextLine();
         System.out.println(str2);
