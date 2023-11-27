16f4c2ef6a351cbc624461af46b7425894e061cd
shameer2762 <shaikshameer2762@gmail.com>
2023-11-27 12:35:29 +0530

    sum of three numbers program

---
+++ SumOfThreeNumbers.java
@@ -0,0 +1,19 @@
+package Java_EdNTech360;
+import java.util.Scanner;
+
+public class SumOfThreeNumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number1 : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter number2 : ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter number3 : ");
+        int num3 = sc.nextInt();
+
+        System.out.printf("%d+%d+%d = %d",num1,num2,num3,num1+num2+num3);
+    }
+}
